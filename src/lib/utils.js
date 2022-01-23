f8a38bacd0546546862ebf8548e214726e8222d4
Matyas Mihalyi <matyas.mihalyi@decathlon.com>
2022-01-23 17:45:42 +0100

    Created getIds function to generate random ids to fetch

---
+++ src/lib/utils.js
@@ -0,0 +1,30 @@
+const max_num_of_ids = 101000;
+const id_length = 6;
+
+
+export function getIds (count=10) {
+  const ids = new Array();
+
+  for (let i = 0; i < count; i++) {
+    ids.push(Math.random() * (max_num_of_ids - 1) + 1)
+  };
+
+  ids.forEach((id, i) => {
+    ids[i] = addZeroes(id);
+  });
+
+  return ids;
+}
+
+function addZeroes (id=1) {
+  const length = id.toString().length;
+  const zeroes = [];
+
+  if (length < id_length) {
+    for (let i = 0; i < id_length - length; i++) {
+      zeroes.push('0');
+    }
+  };
+
+  return `${zeroes.join()}${id}`;
+}

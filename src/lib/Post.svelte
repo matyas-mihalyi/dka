234ab564157fcc5a465a9c60c095891af6d7da0d
Matyas Mihalyi <matyas.mihalyi@decathlon.com>
2022-01-25 22:13:37 +0100

    Added basic style to home feed
    added global css file
    
    added basic styling in Post.svelte

--- src/lib/Post.svelte
+++ src/lib/Post.svelte
@@ -12,12 +12,19 @@
   article {
     width: 100%;
     padding: 1em;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 1em;
+    border-bottom: 1px solid #ccc;
   }
 
   img {
     object-fit: contain;
     width: 100%;
     height: auto;
+    border-radius: 0.075em;
   }
 
 </style>

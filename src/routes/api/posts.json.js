da1a6f5f421a5683b69e19eccfd76e90bdfcffbb
Matyas Mihalyi <matyas.mihalyi@decathlon.com>
2022-01-28 19:31:03 +0100

    posts.json fetches data with a reduce method

--- src/routes/api/posts.json.js
+++ src/routes/api/posts.json.js
@@ -1,25 +1,29 @@
 import { xml2js } from 'xml-js';
 
 import { getPictureUrl, getDescription } from './utils';
+
 import { getIds } from '$lib/utils.js'
 
 export const get = async () => {
   const ids = getIds();
 
-  const promises = ids.map(async (id) => {
-    return fetch(`https://dka.oszk.hu/export/xml_/${id}.xml`)
-    .then(res => res.text())
-    .then(txt => xml2js(txt, {compact: true}))
-  });
-  
-  const posts = await Promise.all(promises.map(async (res) => {
-    console.log(Object.keys((await res).dkalista))
-    return {
-      img: getPictureUrl((await res).dkalista.DKA.identifier.Filename._text, (await res).dkalista.DKA.identifier.URLOfDoc._text),
-      title: (await res).dkalista.DKA.DKAtitle.MainTitle._text,
-      description: getDescription((await res).dkalista.DKA)
+  const posts = await ids.reduce(async (prevPromise, id) => {
+    let posts = await prevPromise;
+    const res = await fetch(`https://dka.oszk.hu/export/xml_/${id}.xml`);
+    console.log(res.ok)
+    if (res.ok) {
+      const text = await res.text();
+      const js = xml2js(text, {compact: true});
+      const post = {
+        img: getPictureUrl(js.dkalista.DKA.identifier.Filename._text, js.dkalista.DKA.identifier.URLOfDoc._text),
+        title: js.dkalista.DKA.DKAtitle.MainTitle._text,
+        description: getDescription(js.dkalista.DKA, { truncated: true})
+      }
+      posts.push(post);
     }
-  }));
+    return posts;
+  }, []);
+
 
   return {
     status: 200,

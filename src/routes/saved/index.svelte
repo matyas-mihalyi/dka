20793255b8891464023686d617ea79ffbb3119eb
Matyas Mihalyi <matyas.mihalyi@decathlon.com>
2022-03-11 20:44:03 +0100

    added route for saved posts

---
+++ src/routes/saved/index.svelte
@@ -0,0 +1,25 @@
+<script context="module">
+  
+  export async function load ({fetch}) {
+
+    const res = await fetch(`/api/saved-posts.json`);
+    const {posts, ids} = await res.json();
+
+    return {
+      props: {
+        posts,
+        ids
+      }
+    }
+  }
+</script>
+
+<script>
+  import Post from '$lib/components/Post.svelte'
+  export let posts;
+
+</script>
+
+{#each posts as post}
+<Post post={post} />
+{/each}

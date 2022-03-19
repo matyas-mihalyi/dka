650172047799e19c02cc54e1316f0651d78cdebe
Matyas Mihalyi <matyas.mihalyi@decathlon.com>
2022-03-19 15:25:25 +0100

    improved getIds function
    the function takes an array of unavailable ids
    
    the function will not return unavailable ids
    
    the function will not return the same ids in one call

--- src/routes/api/utils.js
+++ src/routes/api/utils.js
@@ -31,14 +31,22 @@ export function getDescription (obj = {}, param = { truncated: false}) {
 
 const MAX_NUM_OF_IDS = 101000;
 const ID_LENGTH = 6;
-export function getIds (count=10) {
+
+export function getIds (count=10, unavailableIds=[]) {
   const ids = new Array();
+  const randomNumber = () => Math.floor(Math.random() * (MAX_NUM_OF_IDS - 1) + 1).toString();
+  const getRandomId = () => addZeroes(randomNumber());
+
   for (let i = 0; i < count; i++) {
-    ids.push(Math.floor(Math.random() * (MAX_NUM_OF_IDS - 1) + 1).toString())
+    const newId = getRandomId();
+    if (isUnavailable(newId, unavailableIds)) {
+      i--;
+    } else {
+      ids.push(newId);
+      unavailableIds.push(newId);
+    }
   };
-  ids.forEach((id, i) => {
-    ids[i] = addZeroes(id);
-  });
+
   return ids;
 };
 
@@ -52,6 +60,10 @@ function addZeroes (id=1) {
   };
   return `${zeroes.join('')}${id}`;
 };
+
+function isUnavailable (id='000001', unavailableIds=[]) {
+  return unavailableIds.includes(unavailableId => unavailableId === id);
+}
 
 const CHAR_LIMIT = 140; 
 function truncateDesc (str="") {

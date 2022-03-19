a4bc4a67111d678667a3a3b0a3868a250ee6e9ca
Matyas Mihalyi <matyas.mihalyi@decathlon.com>
2022-03-19 18:48:55 +0100

    post api respond is determined by request body and cookies

--- src/routes/api/posts.json.js
+++ src/routes/api/posts.json.js
@@ -1,19 +1,25 @@
 import { xml2js } from 'xml-js';
-import * as cookieParser from 'cookie-parser';
-
 import { getPictureUrl, getDescription, getIds, getCookie } from './utils';
 
-
 export const post = async ({ request }) => {
+
   const body = JSON.parse(await request.text());
   const numberOfRequestedPosts = await body.number_of_posts;
+  const loadedPosts = await body.loaded_posts;
+
   console.log(numberOfRequestedPosts)
   
   const cookies = request.headers.get('cookie');
   const previousIds = await JSON.parse(getCookie(cookies, 'ids'));
   
-  const ids = previousIds || getIds();
-  const _ids = JSON.stringify(ids);
+  let ids;
+  if (previousIds && !loadedPosts) {
+    ids = previousIds;
+  } else {
+    ids = getIds(numberOfRequestedPosts, loadedPosts);
+  } 
+
+  const idString = JSON.stringify(ids);
   
   const posts = await ids.reduce(async (prevPromise, id) => {
     let posts = await prevPromise;
@@ -32,7 +38,7 @@ export const post = async ({ request }) => {
     return posts;
   }, []);
 
-  if (previousIds) {
+  if (previousIds && !loadedPosts) {
     return {
       status: 200,
       body: {
@@ -44,7 +50,7 @@ export const post = async ({ request }) => {
     return {
       status: 200,
       headers: {
-        'Set-Cookie': `ids=${_ids}; SameSite=Strict; HttpOnly; Secure; path=/`
+        'Set-Cookie': `ids=${idString}; SameSite=Strict; HttpOnly; Secure; path=/`
       },
       body: {
         posts,
@@ -52,6 +58,5 @@ export const post = async ({ request }) => {
       }
     };
   }
-
 
 }

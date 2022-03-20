fe5e3a5b4c7584d46577e3f11d1ad597f6fb0d7f
Matyas Mihalyi <matyas.mihalyi@decathlon.com>
2022-03-20 09:01:10 +0100

    added dummy footer to layout for intersection observer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -5,3 +5,9 @@
 <Menu />
 
 <slot></slot>
+
+
+<footer>
+  <h2>This is a footer</h2>
+  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto at exercitationem, quaerat nisi itaque minus qui officia a praesentium odit veritatis! Non necessitatibus doloremque numquam, cupiditate ullam iure corrupti totam eveniet qui sapiente dignissimos, aliquid praesentium quia molestiae asperiores placeat!</p>
+</footer>

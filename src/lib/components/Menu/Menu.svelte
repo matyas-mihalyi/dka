a5615cb36b16d26f61dbf2a43f2ae55cd2f7d058
Matyas Mihalyi <matyas.mihalyi@decathlon.com>
2022-04-01 21:04:38 +0200

    refactor: remove unnecessary div

--- src/lib/components/Menu/Menu.svelte
+++ src/lib/components/Menu/Menu.svelte
@@ -7,12 +7,20 @@
   import { PROJECT_NAME } from '$lib/config/general';
   
   let isOpen = false;
+  let isMobile = true;
 
   function toggleMenu () {
     isOpen = !isOpen;
+  };
+
+  function checkScreenWidth () {
+    if (window.innerWidth > 600) {
+      isMobile = false;
+      isOpen = true;
+    }
   }
 
-  
+
   let hidden = false;
   
   let y = 0;
@@ -26,36 +34,39 @@
       } else {
         hidden = true;
       }
-    lastY = y;
+      lastY = y;
     }; 
+    checkScreenWidth();
   });
-
+  
 </script>
+
+<style lang="less">
+  @import './Menu';
+</style>
 
 <svelte:window bind:scrollY="{y}"/>
 
 <div role="navigation" class:hidden={hidden}>
 
-  <div class="top-navbar">
-    <a class="logo" href="/">
-      <Icon 
-        name={"dark"} 
-        width={"1.5rem"}
-        height={"1.5rem"}  
-      />
-      <span>
-        {PROJECT_NAME}
-      </span>
-    </a>
-    
-    <button on:click="{toggleMenu}">
-      {#if isOpen}
-        <i class="ri-close-line"></i>
-      {:else}
-        <i class="ri-menu-line"></i>
-      {/if}
-    </button>
-  </div>
+  <a class="logo" href="/">
+    <Icon 
+      name={"dark"} 
+      width={isMobile ? "1.5rem" : "2.5rem"}
+      height={isMobile ? "1.5rem" : "2.5rem"}  
+    />
+    <span>
+      {PROJECT_NAME}
+    </span>
+  </a>
+  
+  <button on:click="{toggleMenu}">
+    {#if isOpen}
+    <i class="ri-close-line"></i>
+    {:else}
+    <i class="ri-menu-line"></i>
+  {/if}
+  </button>
   
   {#if isOpen}
   <nav transition:slide>
@@ -68,68 +79,3 @@
   {/if}
 
 </div>
-
-<style>
-  div[role="navigation"] {
-    position: fixed;
-    top: 0;
-    right: 0;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: flex-end;
-    align-content: flex-end;
-    background-color: #fff;
-    transition: .3s;
-
-    box-shadow: 2px 2px 5px rgba(0, 0, 0, .1);
-  }
-
-  div.top-navbar {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  
-  button {
-    border-radius: 0.125em;
-    border: none;
-    padding: 1em;
-    display: block;
-    background-color: transparent;
-  }
-  
-  button > i {
-    font-size: 1.5rem;
-  }
-
-  nav {
-    width: 100%;
-  }
-  
-  ul {
-    display: flex;
-    flex-direction: column;
-    padding: 1em 1.5em;
-  }
-
-  .hidden {
-    transform: translateY(calc(-100% - 5px));
-  }
-
-  a.logo {
-    padding-left: 1em;
-    display: flex;
-    align-items: center;
-    gap: 0.5em;
-    text-decoration: none;
-  }
-
-  a.logo > span {
-    /* font-size: large; */
-    font-weight: 700;
-    color: #000;
-  }
-
-</style>

2496f08266eb42d5a8cc1bc93c18b940082f907f
Matyas Mihalyi <matyas.mihalyi@decathlon.com>
2022-04-08 22:14:12 +0200

    feat: add vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import vercel from '@sveltejs/adapter-vercel';
 import {resolve} from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -12,6 +13,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
+		adapter: vercel(),
 		vite : {
 			resolve: {
 				alias: {

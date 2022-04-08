04260e97ee4d420c7e24c9a0507bd35dacf8256e
Matyas Mihalyi <matyas.mihalyi@decathlon.com>
2022-04-08 21:04:32 +0200

    feat: add SEO component
    * Seo component adds meta tags to head
    
    * props are declared route level

--- src/lib/components/Common/Seo/Seo.svelte
+++ src/lib/components/Common/Seo/Seo.svelte
@@ -1,33 +1,31 @@
 <script>
-  import { PROJECT_NAME, META_DESCRIPTION } from '$lib/config/general'
+  import { TITLE_SUFFIX } from '$lib/config/general'
 
   export let title;
-  export let description = META_DESCRIPTION;
+  export let description;
   export let url;
   export let contentType;
   export let image;
 
-  const metaTitle = `${title} | ${PROJECT_NAME}`;
-
+  const metaTitle = `${title} ${TITLE_SUFFIX}`;
 
 </script>
 
 <svelte:head>
   <title>{metaTitle}</title>
-  <meta type="description" content="{description}"/>
+  <meta name="description" content="{description}"/>
   <meta
     name="robots"
     content="index, follow, max-snippet:-1, max-image-preview:large"
   />
-  <html lang="hu">
-
+  
   <!-- twitter -->
   <meta name="twitter:card" content="summary_large_image" />
   <meta name="twitter:title" content={metaTitle} />
   <meta name="twitter:description" content={description} />
   <meta name="twitter:url" content={url} />
   {#if image}
-    <meta name="twitter:image" content={image.url} />
+  <meta name="twitter:image" content={image.url} />
     <meta name="twitter:image:alt" content={image.alt} />
   {/if}
 
@@ -37,5 +35,5 @@
   <meta property="og:title"  content="{metaTitle}" />
   <meta property="og:description" content="{description}" />
   <meta property="og:image" itemprop="image" content="{image.url}" />
-
+  
 </svelte:head>

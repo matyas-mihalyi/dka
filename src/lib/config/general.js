aa78dc9312e96cb78815cedcc1887ab38b96d838
Matyas Mihalyi <matyas.mihalyi@decathlon.com>
2022-04-09 09:37:06 +0200

    fix: typos in texts

--- src/lib/config/general.js
+++ src/lib/config/general.js
@@ -1,4 +1,4 @@
-export const PROJECT_NAME = "Képtár nezegtő";
+export const PROJECT_NAME = "Képtár nézegető";
 
 export const TITLE_SUFFIX = ` - ${PROJECT_NAME}`
 export const META_DESCRIPTION = "Egy felhasználóbarát felület a Digitális Képarchívum böngészéséhez.";

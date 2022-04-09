447fdf2194c62ba129685c047cf3517bbe8b49a8
Matyas Mihalyi <matyas.mihalyi@decathlon.com>
2022-04-09 08:59:58 +0200

    fix: preprocessor config

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,17 @@
-import preprocess from 'svelte-preprocess';
+import sveltePreprocess from 'svelte-preprocess';
 import vercel from '@sveltejs/adapter-vercel';
 import {resolve} from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess({
-		less: {
-			prependData: `@import 'src/styles/variables.less';`
-		}
-	}),
+	preprocess: [
+		sveltePreprocess({
+			less: {
+				prependData: `@import 'src/styles/variables.less';`
+			}
+
+		})
+	], 
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html

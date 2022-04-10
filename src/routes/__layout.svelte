66142497e34b21eaf566baf38fb8b817ae1d3b78
Matyas Mihalyi <matyas.mihalyi@decathlon.com>
2022-04-10 18:35:09 +0200

    feat: add css colour variables
    * removed colour variables from variables.less
    
    * added css variables to components' less files
    
    * added wrapper div in layout for themeing

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,17 +1,19 @@
 <script>
   import Menu from '$lib/components/Menu/Menu.svelte';
   import Footer from '$lib/components/Footer/Footer.svelte';
-  import Alert from '$lib/components/Common/Alert/Alert.svelte'
+  import Alert from '$lib/components/Common/Alert/Alert.svelte';
+  import { theme } from '$lib/components/stores/theme';
 </script>
 
 <style>
   @import '$lib/styles/global.css';
 </style>
-
-<Menu />
-
-<slot></slot>
-
-<Footer />
-
-<Alert />
+<div id="app-wrapper" class="{$theme}">
+  <Menu />
+  
+  <slot></slot>
+  
+  <Footer />
+  
+  <Alert />
+</div>

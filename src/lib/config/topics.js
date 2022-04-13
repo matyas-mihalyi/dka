6fddf27ee670d5186cda987129d29afe1d457a03
Matyas Mihalyi <matyas.mihalyi@decathlon.com>
2022-04-13 17:19:49 +0200

    feat: add topics to posts
    * add topic section to posts
    
    * add posts to topic endpoint
    
    * add meta tags to topic endpoint

---
+++ src/lib/config/topics.js
@@ -0,0 +1,10 @@
+const META_DESCRIPTION = "A Digitális Képarchívum ehhez kapcsolódó képei: ";
+const TYPE = "website"
+
+export const TOPIC_FEED_SEO = {
+  description: META_DESCRIPTION,
+  contentType: TYPE,
+  image: {
+    alt: "DKA logo"
+  }
+}
--- src/routes/topic/[topic].svelte
+++ src/routes/topic/[topic].svelte
@@ -17,15 +17,40 @@
 
 </script>
 <script>
+  import {TOPIC_FEED_SEO} from '$lib/config/topics';
+  import { page } from '$app/stores';
+
   import Post from '$lib/components/Post/Post.svelte';
+  import Seo from '$lib/components/Common/Seo/Seo.svelte';
 
   export let posts;
   export let topic;
+
+  TOPIC_FEED_SEO.description = TOPIC_FEED_SEO.description + topic;
+  const { description, contentType, image } = TOPIC_FEED_SEO;
+  const url = $page.url.href;
+  
+
 </script>
 
+<style>
+  h1 {
+    margin-top: 2.5em;
+  }
+</style>
+
 <main class="container">
-  <h1>{topic}</h1>
+  <h1>#{topic}</h1>
   {#each posts as post}
     <Post post={post} />
   {/each}
 </main>
+
+
+<Seo 
+  title={topic}
+  {description}
+  {url}
+  {contentType}
+  {image}
+/>

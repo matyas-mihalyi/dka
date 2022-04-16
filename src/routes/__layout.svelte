5ba079567a2a7f3cfbe5cf9e4c32771d0a6ffc67
Matyas Mihalyi <matyas.mihalyi@decathlon.com>
2022-04-16 12:39:27 +0200

    feat: loading screen
    * add loading screen to layout
    
    * loading screen is triggered when navigating store is truthy

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,11 @@
 <script>
+  import { theme } from '$lib/components/stores/theme';
+  import { navigating } from '$app/stores';
+  import {fade} from 'svelte/transition';
   import Menu from '$lib/components/Menu/Menu.svelte';
   import Footer from '$lib/components/Footer/Footer.svelte';
   import Alert from '$lib/components/Common/Alert/Alert.svelte';
-  import { theme } from '$lib/components/stores/theme';
+  import Loading from '$lib/components/Loading/Loading.svelte';
 </script>
 
 <style>
@@ -11,8 +14,11 @@
 <div id="app-wrapper" class="{$theme}">
   <Menu />
   
+  {#if $navigating}
+  <Loading />
+  {:else}
   <slot></slot>
-  
+  {/if}
   <Footer />
   
   <Alert />

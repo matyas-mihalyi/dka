4516b18980e1f2f396a713b942685402429d7be4
Matyas Mihalyi <matyas.mihalyi@decathlon.com>
2022-04-23 09:27:58 +0200

    fix: incorrect back navigation on post
    Instead of relying on browser history, the href of the back button is the last feed's path.
    
    When a new feed is added, it's path should be added to the regex in getPreviousFeedPath utility function.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,16 +1,20 @@
 <script>
   import { theme } from '$lib/components/stores/theme';
-  import { navigating } from '$app/stores';
-  import {fade} from 'svelte/transition';
+  import { navigating, page } from '$app/stores';
   import Menu from '$lib/components/Menu/Menu.svelte';
   import Footer from '$lib/components/Footer/Footer.svelte';
   import Alert from '$lib/components/Common/Alert/Alert.svelte';
   import Loading from '$lib/components/Loading/Loading.svelte';
+  import {updateHistory} from '$lib/utils'
+
+  $: $page, updateHistory($page.url.pathname);
+
 </script>
 
 <style>
   @import '$lib/styles/global.css';
 </style>
+
 <div id="app-wrapper" class="{$theme}">
   <Menu />
   
